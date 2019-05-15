4d77db82e00fd49ba6e2c4d5b7205eddd79d6aec
Noah Kinslow <nedthenetworker155@gmail.com>
2019-05-15 17:40:51 -0500

    Add basics of gamecore
    Add files for gamecore classes.
    Add first implementation of tile and tower classes.

---
+++ src/app/gamecore/tile.ts
@@ -0,0 +1,63 @@
+import { Builder } from './builder'
+import { Tower } from './tower'
+
+class Tile {
+    
+    builder: Builder | null = null;
+    tower: Tower | null = null;
+
+    constructor() {
+    }
+
+    getBuilder() {
+        if (this.builder) {
+            return this.builder;
+        }
+    }
+
+    setBuilder(newBuilder: Builder | null) {
+        this.builder = newBuilder;
+    }
+
+    getTower() {
+        if (this.tower) {
+            return this.tower;
+        }
+    }
+
+    setTower(newTower: Tower | null) {
+        this.tower = newTower;
+    }
+
+    doesTileHaveBuilder () {
+        if (this.builder) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    doesTileHaveTower () {
+        if (this.tower) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    isTileBlocked() {
+        if (this.tower) {
+            if (this.tower.isTowerFull) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ src/app/gamecore/tower.ts
@@ -0,0 +1,53 @@
+class Tower {
+    height: number = 0;
+
+    constructor() {
+
+    }
+    
+    getHeight() {
+        if (this.height) {
+            return this.height;
+        }
+    }
+
+    setHeight(newHeight: number = 0) {
+        this.height = newHeight;
+    }
+    
+    isTowerEmpty () {
+        if (this.height == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    isTowerFull() {
+        if (this.height == 4) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    incrementHeight() {
+        this.height++;
+    }
+
+    // Add a layer to the tower. True on success, False on failure
+    addLayer() {
+        if (!this.isTowerFull) {
+            this.incrementHeight;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+
+export { Tower };
