6ec64a9b0a8508f9d1fe254f1c551cb3c86ce87d
Noah Kinslow <nedthenetworker155@gmail.com>
2019-05-15 21:14:03 -0500

    Add moveBuilder function

--- src/app/gamecore/torriEngine.ts
+++ src/app/gamecore/torriEngine.ts
@@ -2,17 +2,25 @@ import { Builder } from './builder'
 import { Board } from './board'
 import { Tile } from './tile'
 
+
 function moveBuilder(board: Board, builder: Builder, tileFromName: string, tileToName: string) {
     // Get tiles from map
     let tileFrom = board.getBoardMap().get(tileFromName);
     let tileTo = board.getBoardMap().get(tileToName);
-    // Update builder location
-    builder.setCurrentTile(tileToName);
-    if (tileFrom) {
-        tileFrom.builder = null;
-    }
     if (tileTo) {
-        tileTo.setBuilder(builder);
+        // Check that the tile is not blocked
+        if (!tileTo.isTileBlocked()) {
+            // Update builder location
+            builder.setCurrentTile(tileToName);
+            if (tileFrom) {
+                tileFrom.setBuilder(null);
+            }
+            tileTo.setBuilder(builder);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    
+    return false;
 }
